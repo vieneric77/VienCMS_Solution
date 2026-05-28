f1404755b7fa6c2ba116562769d14195b004c297
vieneric77 <vieneric77@gmail.com>
2026-05-28 20:58:57 +0700

    Hoàn thành Buổi04

--- CMS.Backend/Controllers/HomeController.cs
+++ CMS.Backend/Controllers/HomeController.cs
@@ -1,28 +1,35 @@
 using CMS.Backend.Models;
+using CMS.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Linq;
+
 
 namespace CMS.Backend.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
-
         public IActionResult Index()
         {
-            return View();
+            var latestPosts = _context.Posts
+                .Include(p => p.Category)
+                .OrderByDescending(p => p.CreatedDate)
+                .Take(3)
+                .ToList();
+            return View(latestPosts);
         }
-
         public IActionResult Privacy()
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
